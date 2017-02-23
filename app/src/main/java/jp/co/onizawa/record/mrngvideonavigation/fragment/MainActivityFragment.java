4d377681f0c805a26238878778e5d5f0a5d70e65
onizawa <onizawa.1122@gmail.com>
2017-02-23 22:44:08 +0900

    Create Project

---
+++ app/src/main/java/jp/co/onizawa/record/mrngvideonavigation/fragment/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package jp.co.onizawa.record.mrngvideonavigation.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jp.co.onizawa.record.mrngvideonavigation.R;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
